07eae40169f29522c0611df580c2794fc7981ea0
yl.su <yl.su@trip.com>
2021-02-05 00:00:04 +0800

    feature: add 28 implement strstr

---
+++ 28-implement-strstr.ts
@@ -0,0 +1,59 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+
+function getNext(str) {
+    const next = [0];
+    let now = 0;
+
+    for (let i = 1; i < str.length;) {
+        if (str[now] === str[i]) {
+            now ++;
+            next[i] = now;
+            i ++
+        } else if(now !== 0){
+            now = next[now - 1]
+        } else {
+            next[i] = 0;
+            i ++;
+        }
+    }
+
+    console.log(next)
+    return next;
+}
+
+var strStr = function(haystack, needle) {
+    if (!needle) return 0;
+    if (!haystack || (needle.length > haystack.length)) return -1;
+
+    const next = getNext(needle);
+    const len = haystack.length;
+
+    let index = 0;
+    let pos = 0;
+
+    for (let i = 0; i < len;) {
+        if (needle[pos] === haystack[i]) {
+            if (pos === needle.length - 1) {
+                index = i - pos;
+                break;
+            }
+            pos ++;
+            i ++;
+        } else {
+            index = -1;
+            if (pos > 0) {
+                pos = next[pos - 1];
+            } else {
+                i ++;
+            }
+        }
+    }
+
+    console.log(index)
+    return index;
+};
+strStr("aabaaabaaac", "aabaaac");
